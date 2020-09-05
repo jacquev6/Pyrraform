16a32686b24a94ad05205d87693b4fb9512c1c31
Vincent Jacques <vincent@vincent-jacques.net>
2020-09-05 15:38:36 +0200

    Run terraform-provider-uptime-robot
    - with appropriate environment
    - to see what it prints during the handshake protocol

--- run.sh
+++ run.sh
@@ -2,7 +2,30 @@
 
 set -o errexit
 
-image=$(docker build -q .)
+do_pre_build=false
+no_cache=""
+while [[ "$#" > 0 ]]
+do
+  case $1 in
+    --verbose)
+      do_pre_build=true
+      ;;
+    --no-cache)
+      no_cache="--no-cache --pull"
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1;;
+  esac
+  shift
+done
+
+if $do_pre_build
+then
+  docker build $no_cache .
+fi
+
+image=$(docker build $no_cache -q .)
 
 echo "Run terraform-provider-pyrraform-test in this shell"
 echo "Code changes made to the library are reloaded between runs"
