f6afbde58ae5b65d045d5a8719874812ad604b01
Vincent Jacques <vincent@vincent-jacques.net>
2020-09-23 19:00:27 +0200

    Avoid CI failures when timestamp's milliseconds round to 1000

--- development/integration-tests/run-test.sh
+++ development/integration-tests/run-test.sh
@@ -5,7 +5,7 @@ set -o verbose
 
 
 /usr/local/bin/terraform-provider-* || true
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 test ! -f terraform.tfstate
 
@@ -18,26 +18,26 @@ test ! -f /terraform-provider-pyrraform-test.log
 test ! -f terraform.tfstate
 
 terraform providers schema -json | python -m json.tool
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 test ! -f terraform.tfstate
 
 terraform plan
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 test ! -f terraform.tfstate
 
 terraform apply
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 grep -v "lineage" terraform.tfstate
 
 terraform destroy -auto-approve
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 grep -v "lineage" terraform.tfstate
 
 terraform plan
-sed "s/^........-......\....://" /terraform-provider-pyrraform-test.log >&2
+sed "s/^........-......\..\{3,4\}://" /terraform-provider-pyrraform-test.log >&2
 rm /terraform-provider-pyrraform-test.log
 grep -v "lineage" terraform.tfstate
